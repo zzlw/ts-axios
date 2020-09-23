07350c65d699049342d7b8469b8efa2e73f346a9
小可爱 <xiaokeai@AndydeMacBook-Pro.local>
2020-09-24 00:10:18 +0800

    inti

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import { bulidURL } from './helpers/url'
+import xhr from './xhr'
+
+function axios (config: AxiosRequestConfig): void {
+  processConfig(config)
+  xhr(config)
+}
+
+function processConfig (config: AxiosRequestConfig): void {
+  config.url = transformUrl(config)
+}
+
+function transformUrl (config: AxiosRequestConfig): string {
+  const { url, params } = config
+  return bulidURL(url, params)
+}
+
+export default axios
